32456ecf011fb49dd1dfdd71a5b8cb4f00a552fe
prakash naidu <prakash.naidu20101995@gmail.com>
2019-03-30 17:08:23 +0530

    lc

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './index.scss';
 import startBackgroundImg from '../../assets/images/bg.jpeg';
+import projectImg from '../../assets/images/projectImg.jpg';
 
 class App extends Component {
   render() {
@@ -14,7 +15,9 @@ class App extends Component {
               <span class="navbar-toggler-icon"></span>
             </button>
             <a aria-label="Yasio" href="#start" target="_self" class="navbar-brand">
-              logo
+              <span className="skyblue">&lt; </span>
+              John
+              <span className="skyblue"> /&gt;</span>
             </a>
             <div id="nav_collapse" class="navbar-collapse collapse">
               <ul class="navbar-nav ml-auto">
@@ -53,32 +56,31 @@ class App extends Component {
           </nav>
         </header>
 
-        <div id="start" class="navbar-offset-top min100vh d-flex top-jumbotron pr container-fluid" style={{ backgroundImage: `url(${startBackgroundImg})` }}>
+        <div id="start" class="navbar-offset-top min100vh d-flex background-center" style={{ backgroundImage: `url(${startBackgroundImg})` }}>
           <div className="container-fluid">
-            <div className="row align-self-center align-items-center justify-content-center">
+            <div className="row height100 align-self-center align-items-center justify-content-center">
               <div className="col-sm-7 col-md-7 col-9">
-                <div class="timeline">
-                  <div class="bullet"></div>
-                  <a href="#work" aria-label="Go to next section" class="scroll-bottom scale-hover">
-                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 18 30" >
-                      <path fill="#988E9F" d="M9,30c-5,0-9-4.2-9-9.4V9.4C0,4.2,4,0,9,0s9,4.2,9,9.4v11.3C18,25.8,14,30,9,30z M16.2,9.4	c0-4.2-3.2-7.5-7.2-7.5S1.8,5.3,1.8,9.4v11.3c0,4.1,3.2,7.5,7.2,7.5s7.2-3.4,7.2-7.5V9.4z M9.2,12.8c-0.5,0-0.9-0.4-0.9-0.9V7.1c0-0.5,0.4-0.9,0.9-0.9c0.5,0,0.9,0.4,0.9,0.9v4.7C10.1,12.3,9.7,12.8,9.2,12.8z" class="st0">
-                      </path>
-                    </svg>
-                    <br />
-                    <span>SCROLL</span>
-                  </a>
-                </div>
-                <div>
-                <small id="first-element" class="block-reveal__text section-title mb-3 pb-3">Start /&gt;</small>
-                </div>
-                
-                <h1 className="title">
-                  Hi, my name is
-                  <strong className="purple"> Prakash Naidu</strong>
-                </h1>
-                <h2 className="subtitle">
-                  I Develop Websites, Webapps & Mobile apps.
+                <div className="wrapper">
+                  <h1 className="title">
+                    lorem ipsum some df
+                  <strong className="skyblue"> John Doe</strong>
+                  </h1>
+                  <h2 className="subtitle font-secondary">
+                    lorem ipsum some dummy, text, dfvddfg.
                 </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="work" class="min100vh container-fluid">
+          <div className="container-fluid">
+            <div className="timeline">
+              <div className="project">
+                <div className="project-index">00</div>
+                <div className="project-name">what is lorem ipsum</div>
+                <div className="project-image" style={{ backgroundImage: `url(${projectImg})` }}></div>
               </div>
             </div>
           </div>
